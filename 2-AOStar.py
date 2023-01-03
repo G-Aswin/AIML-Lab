237d1442e2f6cb8aaf05c0648c4a6531eca32be8
G. Aswin <aswin.g.rns@gmail.com>
2023-01-03 23:49:12 +0530

    short aostar

--- 2-AOStar.py
+++ 2-AOStar.py
@@ -11,37 +11,21 @@ class Graph:
     def applyAOStar(self):        
         self.aoStar(self.start, False)
 
-    def getNeighbors(self, v):    
-        return self.graph.get(v,'')
-    
-    def getStatus(self,v):       
-        return self.status.get(v,0)
-    
-    def setStatus(self,v, val):  
-        self.status[v]=val
-    
-    def getHeuristicNodeValue(self, n):
-        return self.H.get(n,0)    
- 
-    def setHeuristicNodeValue(self, n, value):
-        self.H[n]=value           
-        
-    
     def printSolution(self):
         print("FOR GRAPH SOLUTION, TRAVERSE THE GRAPH FROM THE START NODE:",self.start)
         print("------------------------------------------------------------")
         print(self.solutionGraph)
         print("------------------------------------------------------------")
     
-    def computeMinimumCostChildNodes(self, v):
+    def minChild(self, v):
         minimumCost=0
         mincostNL = []
         flag=True
-        for neighbor in self.getNeighbors(v): 
+        for neighbor in self.graph.get(v,''): 
             cost=0
             nodeList=[]
             for c, weight in neighbor:
-                cost=cost+self.getHeuristicNodeValue(c)+weight
+                cost+=self.H.get(c, 0)+weight
                 nodeList.append(c)
             
             if flag==True or minimumCost > cost:                     
@@ -59,30 +43,31 @@ class Graph:
         print("PROCESSING NODE   :", v)
         print("-----------------------------------------------------------------------------------------")
         
-        if self.getStatus(v) >= 0:      
-            minCost, childNL = self.computeMinimumCostChildNodes(v)
-            self.setHeuristicNodeValue(v, minCost)
-            self.setStatus(v,len(childNL))
+        if self.getStatus(v) >= 0:    
+            # update heuristic  
+            minCost, childNL = self.minChild(v)
+            self.H[v] = minCost
+            self.status[v] = len(childNL)
             
+            # Check for solved node
             solved=True                    
             for childNode in childNL:
                 self.parent[childNode]=v
-                if self.getStatus(childNode)!=-1:
+                if self.status.get(childNode, 0) != -1:
                     solved=False
-            
             if solved==True:           
-                self.setStatus(v,-1)    
+                self.status[v] = -1   
                 self.solutionGraph[v]=childNL 
             
-            
+            # Backtrack
             if v!=self.start:          
                 self.aoStar(self.parent[v], True) 
                 
+            # Forward movement
             if backTracking==False:   
                 for childNode in childNL:  
-                    self.setStatus(childNode,0)  
+                    self.status[childNode] = 0  
                     self.aoStar(childNode, False)
-                 
         
                                        
 h1 = {'A': 1, 'B': 6, 'C': 2, 'D': 12, 'E': 2, 'F': 1, 'G': 5, 'H': 7, 'I': 7, 'J': 1, 'T': 3}
@@ -97,13 +82,13 @@ G1= Graph(graph1, h1, 'A')
 G1.applyAOStar() 
 G1.printSolution()
 
-h2 = {'A': 1, 'B': 6, 'C': 12, 'D': 10, 'E': 4, 'F': 4, 'G': 5, 'H': 7}  
-graph2 = {                                        
-    'A': [[('B', 1), ('C', 1)], [('D', 1)]],     
-    'B': [[('G', 1)], [('H', 1)]],               
-    'D': [[('E', 1), ('F', 1)]]                  
-}
+# h2 = {'A': 1, 'B': 6, 'C': 12, 'D': 10, 'E': 4, 'F': 4, 'G': 5, 'H': 7}  
+# graph2 = {                                        
+#     'A': [[('B', 1), ('C', 1)], [('D', 1)]],     
+#     'B': [[('G', 1)], [('H', 1)]],               
+#     'D': [[('E', 1), ('F', 1)]]                  
+# }
 
-G2 = Graph(graph2, h2, 'A')                     
-G2.applyAOStar()                                
-G2.printSolution()                              
+# G2 = Graph(graph2, h2, 'A')                     
+# G2.applyAOStar()                                
+# G2.printSolution()                              
